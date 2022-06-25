ec593cbdc3ae881bc30bd48790a9eae286bfd056
Devesh Ahuja <dahuja2514@gmail.com>
2022-06-25 17:47:15 +0530

    added urls for creating a doctor and getting all doctors

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns=[
-	path('createuser/',views.Createuser,name='createuser')
-
+	# path('createuser/',views.Createuser,name='createuser'),
+	path('doctors/',views.getDoctors),
+	path('create-doctor/',views.CreateDoctorView.as_view())
 ]
