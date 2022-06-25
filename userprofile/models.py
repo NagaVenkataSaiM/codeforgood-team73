67fbd063bc3d09488860d6072a6b80f1923b671e
mayankt18 <mayankt1807@gmail.com>
2022-06-25 19:50:45 +0530

    Update the models in userprofile to add phone number validators

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< Updated upstream
-from django.core.validators import MinValueValidator,MaxValueValidator
-=======
 from django.core.validators import MinValueValidator, MaxValueValidator
 
->>>>>>> Stashed changes
 
 class Invitecodes(models.Model):
     email = models.CharField(max_length=40, null=False, blank=False)
@@ -15,15 +11,6 @@ class Invitecodes(models.Model):
 
 
 class Doctor(models.Model):
-<<<<<<< Updated upstream
-    name = models.CharField(max_length=255,null=True,blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1,primary_key=True)
-    email = models.CharField(max_length=100,null=True,blank=True)
-    phone = models.IntegerField(null=True,blank=True,validators=[MinValueValidator(1000000000),MaxValueValidator(9999999999)])
-    specialization = models.CharField(max_length=255,null=True, blank=True)
-    city = models.CharField(max_length=255,null=True,blank=True)
-    gender = models.CharField(max_length=1,null=True,blank=True)    
-=======
     name = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
@@ -34,6 +21,5 @@ class Doctor(models.Model):
     city = models.CharField(max_length=255, null=True, blank=True)
     gender = models.CharField(max_length=1, null=True, blank=True)
 
->>>>>>> Stashed changes
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
