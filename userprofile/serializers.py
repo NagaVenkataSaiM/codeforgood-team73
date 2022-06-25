e4a8e7e5a34b69e53921e2d309d781f8acb1d426
Devesh Ahuja <dahuja2514@gmail.com>
2022-06-25 19:37:23 +0530

    changed the fields being returned by the serializer

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -3,8 +3,14 @@ from statistics import mode
 from telnetlib import DO
 from rest_framework import serializers
 from .models import Doctor
+from django.contrib.auth.models import User
 
 class DoctorSerializer(serializers.ModelSerializer):
+
+    user = serializers.PrimaryKeyRelatedField(
+        default=serializers.CurrentUserDefault(),
+        queryset=User.objects.all(),
+    )
     class Meta :
         model = Doctor
-        fields = '__all__'
+        fields = ['name','user','email','phone','specialization','city','gender']
