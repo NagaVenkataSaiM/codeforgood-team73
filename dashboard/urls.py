94e334b7f04b2a263f407e1a8bc98fe60057514e
mayankt18 <mayankt1807@gmail.com>
2022-06-26 07:54:26 +0530

    Add urls in dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path,include
-#now import the views.py file into this code
+from django.urls import path, include
+# now import the views.py file into this code
 from . import views
 
-urlpatterns=[
-	path('',views.Home,name='home'),
-	path('signup/',views.Signup,name='signup')
+urlpatterns = [
+    path('', views.Home, name='home'),
+    path('signup/', views.Signup, name='signup'),
 
+    path('create-appointment/', views.create_appointment),
+
+    path('get-appointments/', views.get_appointments),
+
+    path('create-usercounselling/', views.create_usercounselling),
+
+    path('update-usercounselling-status', views.update_usercounselling_status),
+
+    path('get-usercounselling', views.get_usercounselling)
 ]
